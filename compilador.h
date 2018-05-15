3fa7135ec2dfba966e805b47f5533e3751012c65
Barbaragd <galindodorame@gmail.com>
2018-05-14 18:50:56 -0700

    THIS is the library

---
+++ compilador.h
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+class Compiler{
+public:
+    string documentador();
+private:
+
+}
