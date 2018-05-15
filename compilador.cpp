b11c2b2348b0e8a82c8d9587db013526f7d595e0
Barbaragd <galindodorame@gmail.com>
2018-05-14 18:48:45 -0700

    comp library

---
+++ compilador.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include "compilador.h"
+
+using namespace std;
+
+int main(){
+
+    string lex;
+    string file = "test_hw3.clj";
+
+    //cout << "Enter file name... ";
+    //cin >> file;
+    
+    cout << "Press enter to continue...\n";
+    cin.get();
+    return 0;
+}
